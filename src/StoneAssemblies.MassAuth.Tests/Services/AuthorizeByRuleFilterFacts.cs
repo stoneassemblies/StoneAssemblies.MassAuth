6a2f66615868c5f6d09b120769a825db92e0d97e
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-06-19 16:58:58 -0400

    Set value for test message

--- src/StoneAssemblies.MassAuth.Tests/Services/AuthorizeByRuleFilterFacts.cs
+++ src/StoneAssemblies.MassAuth.Tests/Services/AuthorizeByRuleFilterFacts.cs
@@ -28,122 +28,20 @@ namespace StoneAssemblies.MassAuth.Tests.Services
     using Xunit;
 
     /// <summary>
-    /// The authorize by rule filter facts.
+    ///     The authorize by rule filter facts.
     /// </summary>
     public class AuthorizeByRuleFilterFacts
     {
         /// <summary>
-        /// The the_ on action execution async_ method.
+        ///     The the_ on action execution async_ method.
         /// </summary>
         public class The_OnActionExecutionAsync_Method
         {
             /// <summary>
-            /// The invokes next action delegate when the request have no message as parameter.
+            ///     Does not invoke next action delegate when the request is unauthorized.
             /// </summary>
             /// <returns>
-            /// The <see cref="Task"/>.
-            /// </returns>
-            [Fact]
-            public async Task Invokes_Next_Action_Delegate_When_The_Request_Have_No_Message_As_Parameter()
-            {
-                var actionContext = new ActionContext
-                                        {
-                                            HttpContext = new DefaultHttpContext(),
-                                            RouteData = new RouteData(),
-                                            ActionDescriptor = new ActionDescriptor()
-                                        };
-                var actionExecutingContext = new ActionExecutingContext(
-                    actionContext,
-                    new List<IFilterMetadata>(),
-                    new Dictionary<string, object>(),
-                    new Mock<Controller>().Object);
-
-                var clientFactoryMock = new Mock<IClientFactory>();
-
-                var nextInvoked = false;
-                var authorizeByRuleFilter = new AuthorizeByRuleFilter(clientFactoryMock.Object);
-                var actionExecutionDelegate = new ActionExecutionDelegate(
-                    () =>
-                        {
-                            nextInvoked = true;
-                            return Task.FromResult<ActionExecutedContext>(null);
-                        });
-                await authorizeByRuleFilter.OnActionExecutionAsync(actionExecutingContext, actionExecutionDelegate);
-
-                Assert.True(nextInvoked);
-            }
-
-            /// <summary>
-            /// Invokes next action delegate when the request is authorized.
-            /// </summary>
-            /// <returns>
-            /// The <see cref="Task"/>.
-            /// </returns>
-            [Fact]
-            public async Task Invokes_Next_Action_Delegate_When_The_Request_Is_Authorized()
-            {
-                var actionContext = new ActionContext
-                                        {
-                                            HttpContext = new DefaultHttpContext(),
-                                            RouteData = new RouteData(),
-                                            ActionDescriptor = new ActionDescriptor()
-                                        };
-                var actionArguments = new Dictionary<string, object>
-                                          {
-                                              { "accountBalanceRequestMessage", new AccountBalanceRequestMessage() }
-                                          };
-
-                var actionExecutingContext = new ActionExecutingContext(
-                    actionContext,
-                    new List<IFilterMetadata>(),
-                    actionArguments,
-                    new Mock<Controller>());
-
-                var clientFactoryMock = new Mock<IClientFactory>();
-                var requestClientMock =
-                    new Mock<IRequestClient<AuthorizationRequestMessage<AccountBalanceRequestMessage>>>();
-                requestClientMock.Setup(
-                    client => client.GetResponse<AuthorizationResponseMessage>(
-                        It.IsAny<AuthorizationRequestMessage<AccountBalanceRequestMessage>>(),
-                        CancellationToken.None,
-                        default)).ReturnsAsync(
-                    () =>
-                        {
-                            var mock = new Mock<ConsumeContext<AuthorizationResponseMessage>>();
-                            mock.Setup(context => context.Message).Returns(
-                                new AuthorizationResponseMessage
-                                    {
-                                        IsAuthorized = true
-                                    });
-                            var messageResponse = new MessageResponse<AuthorizationResponseMessage>(mock.Object);
-                            return messageResponse;
-                        });
-
-                clientFactoryMock
-                    .Setup(
-                        factory => factory
-                            .CreateRequestClient<AuthorizationRequestMessage<AccountBalanceRequestMessage>>(
-                                It.IsAny<RequestTimeout>())).Returns(requestClientMock.Object);
-
-                var nextInvoked = false;
-                var authorizeByRuleFilter = new AuthorizeByRuleFilter(clientFactoryMock.Object);
-                var actionExecutionDelegate = new ActionExecutionDelegate(
-                    () =>
-                        {
-                            nextInvoked = true;
-                            return Task.FromResult<ActionExecutedContext>(null);
-                        });
-
-                await authorizeByRuleFilter.OnActionExecutionAsync(actionExecutingContext, actionExecutionDelegate);
-
-                Assert.True(nextInvoked);
-            }
-
-            /// <summary>
-            /// Does not invoke next action delegate when the request is unauthorized.
-            /// </summary>
-            /// <returns>
-            /// The <see cref="Task"/>.
+            ///     The <see cref="Task" />.
             /// </returns>
             [Fact]
             public async Task Does_Not_Invoke_Next_Action_Delegate_When_The_Request_Is_Unauthorized()
@@ -156,7 +54,12 @@ namespace StoneAssemblies.MassAuth.Tests.Services
                                         };
                 var actionArguments = new Dictionary<string, object>
                                           {
-                                              { "accountBalanceRequestMessage", new AccountBalanceRequestMessage() }
+                                              {
+                                                  "accountBalanceRequestMessage", new AccountBalanceRequestMessage
+                                                      {
+                                                          PrimaryAccountNumber = "123456789012"
+                                                      }
+                                              }
                                           };
 
                 var actionExecutingContext = new ActionExecutingContext(
@@ -203,6 +106,114 @@ namespace StoneAssemblies.MassAuth.Tests.Services
                 await authorizeByRuleFilter.OnActionExecutionAsync(actionExecutingContext, actionExecutionDelegate);
 
                 Assert.False(nextInvoked);
+            }
+
+            /// <summary>
+            ///     The invokes next action delegate when the request have no message as parameter.
+            /// </summary>
+            /// <returns>
+            ///     The <see cref="Task" />.
+            /// </returns>
+            [Fact]
+            public async Task Invokes_Next_Action_Delegate_When_The_Request_Have_No_Message_As_Parameter()
+            {
+                var actionContext = new ActionContext
+                                        {
+                                            HttpContext = new DefaultHttpContext(),
+                                            RouteData = new RouteData(),
+                                            ActionDescriptor = new ActionDescriptor()
+                                        };
+                var actionExecutingContext = new ActionExecutingContext(
+                    actionContext,
+                    new List<IFilterMetadata>(),
+                    new Dictionary<string, object>(),
+                    new Mock<Controller>().Object);
+
+                var clientFactoryMock = new Mock<IClientFactory>();
+
+                var nextInvoked = false;
+                var authorizeByRuleFilter = new AuthorizeByRuleFilter(clientFactoryMock.Object);
+                var actionExecutionDelegate = new ActionExecutionDelegate(
+                    () =>
+                        {
+                            nextInvoked = true;
+                            return Task.FromResult<ActionExecutedContext>(null);
+                        });
+                await authorizeByRuleFilter.OnActionExecutionAsync(actionExecutingContext, actionExecutionDelegate);
+
+                Assert.True(nextInvoked);
+            }
+
+            /// <summary>
+            ///     Invokes next action delegate when the request is authorized.
+            /// </summary>
+            /// <returns>
+            ///     The <see cref="Task" />.
+            /// </returns>
+            [Fact]
+            public async Task Invokes_Next_Action_Delegate_When_The_Request_Is_Authorized()
+            {
+                var actionContext = new ActionContext
+                                        {
+                                            HttpContext = new DefaultHttpContext(),
+                                            RouteData = new RouteData(),
+                                            ActionDescriptor = new ActionDescriptor()
+                                        };
+
+                var actionArguments = new Dictionary<string, object>
+                                          {
+                                              {
+                                                  "accountBalanceRequestMessage", new AccountBalanceRequestMessage
+                                                      {
+                                                          PrimaryAccountNumber = "123456789012"
+                                                      }
+                                              }
+                                          };
+
+                var actionExecutingContext = new ActionExecutingContext(
+                    actionContext,
+                    new List<IFilterMetadata>(),
+                    actionArguments,
+                    new Mock<Controller>());
+
+                var clientFactoryMock = new Mock<IClientFactory>();
+                var requestClientMock =
+                    new Mock<IRequestClient<AuthorizationRequestMessage<AccountBalanceRequestMessage>>>();
+                requestClientMock.Setup(
+                    client => client.GetResponse<AuthorizationResponseMessage>(
+                        It.IsAny<AuthorizationRequestMessage<AccountBalanceRequestMessage>>(),
+                        CancellationToken.None,
+                        default)).ReturnsAsync(
+                    () =>
+                        {
+                            var mock = new Mock<ConsumeContext<AuthorizationResponseMessage>>();
+                            mock.Setup(context => context.Message).Returns(
+                                new AuthorizationResponseMessage
+                                    {
+                                        IsAuthorized = true
+                                    });
+                            var messageResponse = new MessageResponse<AuthorizationResponseMessage>(mock.Object);
+                            return messageResponse;
+                        });
+
+                clientFactoryMock
+                    .Setup(
+                        factory => factory
+                            .CreateRequestClient<AuthorizationRequestMessage<AccountBalanceRequestMessage>>(
+                                It.IsAny<RequestTimeout>())).Returns(requestClientMock.Object);
+
+                var nextInvoked = false;
+                var authorizeByRuleFilter = new AuthorizeByRuleFilter(clientFactoryMock.Object);
+                var actionExecutionDelegate = new ActionExecutionDelegate(
+                    () =>
+                        {
+                            nextInvoked = true;
+                            return Task.FromResult<ActionExecutedContext>(null);
+                        });
+
+                await authorizeByRuleFilter.OnActionExecutionAsync(actionExecutingContext, actionExecutionDelegate);
+
+                Assert.True(nextInvoked);
             }
         }
     }

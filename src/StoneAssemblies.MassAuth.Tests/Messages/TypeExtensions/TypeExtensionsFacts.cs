45ee0e668c3e14a985b81adadd0cfbabff02606f
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-06-19 12:52:58 -0400

    Add doc to tests

--- src/StoneAssemblies.MassAuth.Tests/Messages/TypeExtensions/TypeExtensionsFacts.cs
+++ src/StoneAssemblies.MassAuth.Tests/Messages/TypeExtensions/TypeExtensionsFacts.cs
@@ -1,4 +1,10 @@
-﻿namespace StoneAssemblies.MassAuth.Tests.Messages.TypeExtensions
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TypeExtensionsFacts.cs" company="Stone Assemblies">
+// Copyright © 2021 - 2021 Stone Assemblies. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace StoneAssemblies.MassAuth.Tests.Messages.TypeExtensions
 {
     using StoneAssemblies.MassAuth.Bank.Messages;
     using StoneAssemblies.MassAuth.Messages;
@@ -6,10 +12,19 @@
 
     using Xunit;
 
+    /// <summary>
+    ///     The type extensions facts.
+    /// </summary>
     public class TypeExtensionsFacts
     {
+        /// <summary>
+        ///     The get flat name method.
+        /// </summary>
         public class The_GetFlatName_Method
         {
+            /// <summary>
+            ///     Returns the expected name.
+            /// </summary>
             [Fact]
             public void Returns_The_Expected_Name()
             {

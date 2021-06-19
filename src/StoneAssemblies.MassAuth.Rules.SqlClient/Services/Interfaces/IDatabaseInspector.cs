fa8cffdff2da33fde70becdb74f0228891c49b75
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-06-19 10:40:43 -0400

    Improve docs and update ignore lists

--- src/StoneAssemblies.MassAuth.Rules.SqlClient/Services/Interfaces/IDatabaseInspector.cs
+++ src/StoneAssemblies.MassAuth.Rules.SqlClient/Services/Interfaces/IDatabaseInspector.cs
@@ -1,11 +1,29 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IDatabaseInspector.cs" company="Stone Assemblies">
+// Copyright © 2021 - 2021 Stone Assemblies. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace StoneAssemblies.MassAuth.Rules.SqlClient.Services.Interfaces
 {
     using System.Collections.Generic;
 
+    /// <summary>
+    /// The DatabaseInspector interface.
+    /// </summary>
     public interface IDatabaseInspector
     {
+        /// <summary>
+        /// Gets the connection string.
+        /// </summary>
         string ConnectionString { get; }
 
+        /// <summary>
+        /// Gets the stored procedures.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable{String}"/>.
+        /// </returns>
         IEnumerable<string> GetStoredProcedures();
     }
 }
--- src/StoneAssemblies.MassAuth.Rules.SqlClient/Services/SqlClientDatabaseInspector.cs
+++ src/StoneAssemblies.MassAuth.Rules.SqlClient/Services/SqlClientDatabaseInspector.cs
@@ -1,3 +1,9 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SqlClientDatabaseInspector.cs" company="Stone Assemblies">
+// Copyright © 2021 - 2021 Stone Assemblies. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace StoneAssemblies.MassAuth.Rules.SqlClient.Services
 {
     using System.Collections.Generic;

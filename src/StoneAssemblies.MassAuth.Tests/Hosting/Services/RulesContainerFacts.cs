c221fdf49b7b109ede9af567aa8cf3063e16ee1b
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-06-25 14:13:41 -0400

    Add tests for rule container and object extensions

---
+++ src/StoneAssemblies.MassAuth.Tests/Hosting/Services/RulesContainerFacts.cs
@@ -0,0 +1,82 @@
+﻿namespace StoneAssemblies.MassAuth.Tests.Hosting.Services
+{
+    using System.Collections.Generic;
+
+    using Moq;
+
+    using StoneAssemblies.MassAuth.Bank.Messages;
+    using StoneAssemblies.MassAuth.Hosting.Services;
+    using StoneAssemblies.MassAuth.Messages;
+    using StoneAssemblies.MassAuth.Rules.Interfaces;
+
+    using Xunit;
+
+    /// <summary>
+    ///     The rules container facts.
+    /// </summary>
+    public class RulesContainerFacts
+    {
+        /// <summary>
+        ///     The rules property.
+        /// </summary>
+        public class The_Rules_Property
+        {
+            /// <summary>
+            ///     Ignores disable rules.
+            /// </summary>
+            [Fact]
+            public void Ignores_Disable_Rules()
+            {
+                var rules = new List<IRule<AuthorizationRequestMessage<AccountBalanceRequestMessage>>>();
+
+                var ruleAMock = new Mock<IRule<AuthorizationRequestMessage<AccountBalanceRequestMessage>>>();
+                ruleAMock.SetupGet(rule => rule.IsEnabled).Returns(true);
+                rules.Add(ruleAMock.Object);
+
+                var ruleBMock = new Mock<IRule<AuthorizationRequestMessage<AccountBalanceRequestMessage>>>();
+                ruleBMock.SetupGet(rule => rule.IsEnabled).Returns(false);
+                rules.Add(ruleBMock.Object);
+
+                var rulesContainer = new RulesContainer<AuthorizationRequestMessage<AccountBalanceRequestMessage>>(rules);
+
+                Assert.Single(rulesContainer.Rules);
+            }
+
+            /// <summary>
+            ///     Sorts rules by priority.
+            /// </summary>
+            [Fact]
+            public void Sorts_Rules_By_Priority()
+            {
+                var rules = new List<IRule<AuthorizationRequestMessage<AccountBalanceRequestMessage>>>();
+
+                var ruleAMock = new Mock<IRule<AuthorizationRequestMessage<AccountBalanceRequestMessage>>>();
+                ruleAMock.SetupGet(rule => rule.IsEnabled).Returns(true);
+                ruleAMock.SetupGet(rule => rule.Priority).Returns(2);
+                rules.Add(ruleAMock.Object);
+
+                var ruleBMock = new Mock<IRule<AuthorizationRequestMessage<AccountBalanceRequestMessage>>>();
+                ruleBMock.SetupGet(rule => rule.IsEnabled).Returns(true);
+                ruleBMock.SetupGet(rule => rule.Priority).Returns(1);
+                rules.Add(ruleBMock.Object);
+
+                var ruleCMock = new Mock<IRule<AuthorizationRequestMessage<AccountBalanceRequestMessage>>>();
+                ruleCMock.SetupGet(rule => rule.IsEnabled).Returns(true);
+                ruleCMock.SetupGet(rule => rule.Priority).Returns(0);
+                rules.Add(ruleCMock.Object);
+
+                var ruleDMock = new Mock<IRule<AuthorizationRequestMessage<AccountBalanceRequestMessage>>>();
+                ruleDMock.SetupGet(rule => rule.IsEnabled).Returns(true);
+                ruleDMock.SetupGet(rule => rule.Priority).Returns(0);
+                rules.Add(ruleDMock.Object);
+
+                var rulesContainer = new RulesContainer<AuthorizationRequestMessage<AccountBalanceRequestMessage>>(rules);
+
+                for (var i = 0; i < rulesContainer.Rules.Count - 1; i++)
+                {
+                    Assert.True(rulesContainer.Rules[i].Priority <= rulesContainer.Rules[i + 1].Priority);
+                }
+            }
+        }
+    }
+}
---
+++ src/StoneAssemblies.MassAuth.Tests/Rules/ObjectExtensionsFacts.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjectExtensionsFacts.cs" company="Stone Assemblies">
+// Copyright © 2021 - 2021 Stone Assemblies. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace StoneAssemblies.MassAuth.Tests.Rules
+{
+    using StoneAssemblies.MassAuth.Bank.Messages;
+    using StoneAssemblies.MassAuth.Rules.Extensions;
+
+    using Xunit;
+
+    /// <summary>
+    ///     The object extensions facts.
+    /// </summary>
+    public class ObjectExtensionsFacts
+    {
+        /// <summary>
+        ///     The get property value generic method.
+        /// </summary>
+        public class The_GetPropertyValue_Generic_Method
+        {
+            /// <summary>
+            ///     Returns the value of an object property via reflection.
+            /// </summary>
+            [Fact]
+            public void Return_The_Value_Of_An_Object_Property_Via_Reflection()
+            {
+                var accountBalanceRequestMessage = new AccountBalanceRequestMessage();
+                var expectedValue = accountBalanceRequestMessage.PrimaryAccountNumber = "123456789";
+
+                var value = accountBalanceRequestMessage.GetPropertyValue<string>(
+                    nameof(AccountBalanceRequestMessage.PrimaryAccountNumber));
+                Assert.Equal(expectedValue, value);
+            }
+        }
+
+        /// <summary>
+        ///     The get property value  method.
+        /// </summary>
+        public class The_GetPropertyValue_Method
+        {
+            /// <summary>
+            ///     Returns the value of an object property via reflection.
+            /// </summary>
+            [Fact]
+            public void Return_The_Value_Of_An_Object_Property_Via_Reflection()
+            {
+                var accountBalanceRequestMessage = new AccountBalanceRequestMessage();
+                var expectedValue = accountBalanceRequestMessage.PrimaryAccountNumber = "123456789";
+
+                var value = accountBalanceRequestMessage.GetPropertyValue(nameof(AccountBalanceRequestMessage.PrimaryAccountNumber));
+                Assert.Equal(expectedValue, value);
+            }
+        }
+    }
+}

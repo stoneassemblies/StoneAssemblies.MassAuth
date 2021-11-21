dd9147d0a514c7dc396ae01950d0f53a84c69a34
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-11-20 19:02:16 -0500

    Improve GetMessages implementation

---
+++ src/StoneAssemblies.MassAuth.Tests/Extensions/ActionExecutingContextExtensionsFacts.cs
@@ -0,0 +1,197 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ActionExecutingContextExtensionsFacts.cs" company="Stone Assemblies">
+// Copyright Â© 2021 - 2021 Stone Assemblies. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace StoneAssemblies.MassAuth.Tests.Extensions
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Abstractions;
+    using Microsoft.AspNetCore.Mvc.Controllers;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.AspNetCore.Routing;
+
+    using Moq;
+
+    using StoneAssemblies.MassAuth.Bank.Messages;
+    using StoneAssemblies.MassAuth.Extensions;
+    using StoneAssemblies.MassAuth.Services.Attributes;
+
+    using Xunit;
+
+    public class ActionExecutingContextExtensionsFacts
+    {
+        /// <summary>
+        ///     The get messages method.
+        /// </summary>
+        public class The_GetMessages_Method
+        {
+            /// <summary>
+            ///     The controller method.
+            /// </summary>
+            /// <param name="primaryAccountNumber">
+            ///     The primary account number.
+            /// </param>
+            [AuthorizeByRule(typeof(AccountBalanceRequestMessage))]
+            public void ControllerMethod(string primaryAccountNumber)
+            {
+            }
+
+            /// <summary>
+            ///     The controller method 2.
+            /// </summary>
+            /// <param name="primaryAccountNumber">
+            ///     The primary account number.
+            /// </param>
+            [AuthorizeByRule]
+            public void ControllerMethod2(string primaryAccountNumber)
+            {
+            }
+
+            /// <summary>
+            ///     Does not return messages from action attribute when messages types are not available.
+            /// </summary>
+            [Fact]
+            public void Does_Not_Return_Messages_From_Action_Attribute_When_MessagesTypes_Are_Not_Available()
+            {
+                var actionContext = new ActionContext
+                                        {
+                                            HttpContext = new DefaultHttpContext(),
+                                            RouteData = new RouteData(),
+                                            ActionDescriptor = new ControllerActionDescriptor
+                                                                   {
+                                                                       MethodInfo = this.GetType()
+                                                                           .GetMethod(nameof(this.ControllerMethod2)),
+                                                                   },
+                                        };
+                var actionArguments = new Dictionary<string, object>
+                                          {
+                                              {
+                                                  "PrimaryAccountNumber", "123456789012"
+                                              },
+                                              {
+                                                  "UserName", "Jane"
+                                              },
+                                          };
+
+                var actionExecutingContext = new ActionExecutingContext(
+                    actionContext,
+                    new List<IFilterMetadata>(),
+                    actionArguments,
+                    new Mock<Controller>());
+
+                var messages = actionExecutingContext.GetMessages();
+
+                Assert.Empty(messages);
+            }
+
+            /// <summary>
+            ///     Returns messages from action attribute.
+            /// </summary>
+            [Fact]
+            public void Returns_Messages_From_Action_Attribute()
+            {
+                var actionContext = new ActionContext
+                                        {
+                                            HttpContext = new DefaultHttpContext(),
+                                            RouteData = new RouteData(),
+                                            ActionDescriptor = new ControllerActionDescriptor
+                                                                   {
+                                                                       MethodInfo = this.GetType()
+                                                                           .GetMethod(nameof(this.ControllerMethod)),
+                                                                   },
+                                        };
+                var actionArguments = new Dictionary<string, object>
+                                          {
+                                              {
+                                                  "PrimaryAccountNumber", "123456789012"
+                                              },
+                                          };
+
+                var actionExecutingContext = new ActionExecutingContext(
+                    actionContext,
+                    new List<IFilterMetadata>(),
+                    actionArguments,
+                    new Mock<Controller>());
+
+                var messages = actionExecutingContext.GetMessages();
+
+                Assert.NotEmpty(messages);
+            }
+
+            /// <summary>
+            ///     Returns messages from action attribute ignoring the unmatched arguments.
+            /// </summary>
+            [Fact]
+            public void Returns_Messages_From_Action_Attribute_Ignoring_The_Unmatched_Arguments()
+            {
+                var actionContext = new ActionContext
+                                        {
+                                            HttpContext = new DefaultHttpContext(),
+                                            RouteData = new RouteData(),
+                                            ActionDescriptor = new ControllerActionDescriptor
+                                                                   {
+                                                                       MethodInfo = this.GetType()
+                                                                           .GetMethod(nameof(this.ControllerMethod)),
+                                                                   },
+                                        };
+                var actionArguments = new Dictionary<string, object>
+                                          {
+                                              {
+                                                  "PrimaryAccountNumber", "123456789012"
+                                              },
+                                              {
+                                                  "UserName", "Jane"
+                                              },
+                                          };
+
+                var actionExecutingContext = new ActionExecutingContext(
+                    actionContext,
+                    new List<IFilterMetadata>(),
+                    actionArguments,
+                    new Mock<Controller>());
+
+                var messages = actionExecutingContext.GetMessages();
+
+                Assert.NotEmpty(messages);
+            }
+
+            /// <summary>
+            ///     Returns messages from action message base arguments.
+            /// </summary>
+            [Fact]
+            public void Returns_Messages_From_Action_MessageBase_Arguments()
+            {
+                var actionContext = new ActionContext
+                                        {
+                                            HttpContext = new DefaultHttpContext(),
+                                            RouteData = new RouteData(),
+                                            ActionDescriptor = new ActionDescriptor(),
+                                        };
+                var actionArguments = new Dictionary<string, object>
+                                          {
+                                              {
+                                                  "accountBalanceRequestMessage", new AccountBalanceRequestMessage
+                                                                                      {
+                                                                                          PrimaryAccountNumber = "123456789012",
+                                                                                      }
+                                              },
+                                          };
+
+                var actionExecutingContext = new ActionExecutingContext(
+                    actionContext,
+                    new List<IFilterMetadata>(),
+                    actionArguments,
+                    new Mock<Controller>());
+
+                var messages = actionExecutingContext.GetMessages();
+
+                Assert.NotEmpty(messages);
+            }
+        }
+    }
+}
--- src/StoneAssemblies.MassAuth/Extensions/ActionExecutingContextExtensions.cs
+++ src/StoneAssemblies.MassAuth/Extensions/ActionExecutingContextExtensions.cs
@@ -37,32 +37,43 @@ namespace StoneAssemblies.MassAuth.Extensions
             if (context.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
             {
                 var authorizeByRuleAttribute = controllerActionDescriptor.MethodInfo.GetCustomAttribute<AuthorizeByRuleAttribute>();
-
                 if (authorizeByRuleAttribute?.MessageTypes != null)
                 {
                     foreach (var messageType in authorizeByRuleAttribute.MessageTypes)
                     {
                         var message = Activator.CreateInstance(messageType) as MessageBase;
-                        foreach (var actionArgument in context.ActionArguments)
-                        {
-                            var actionArgumentValue = actionArgument.Value;
-                            if (actionArgumentValue != null)
-                            {
-                                var propertyInfo = messageType.GetProperties(BindingFlags.Instance | BindingFlags.Public)
-                                    .FirstOrDefault(
-                                        p => p.Name.Equals(actionArgument.Key, StringComparison.InvariantCultureIgnoreCase)
-                                             && actionArgumentValue.GetType().IsAssignableFrom(p.PropertyType));
-
-                                propertyInfo?.SetValue(message, actionArgumentValue);
-                            }
-                        }
-
+                        context.Fill(message);
                         messages.Add(message);
                     }
                 }
             }
 
             return messages;
+        }
+
+        /// <summary>
+        ///     Fill a message from the context.
+        /// </summary>
+        /// <param name="context">
+        ///     The context.
+        /// </param>
+        /// <param name="message">
+        ///     The message.
+        /// </param>
+        private static void Fill(this ActionExecutingContext context, MessageBase message)
+        {
+            foreach (var actionArgument in context.ActionArguments)
+            {
+                var actionArgumentValue = actionArgument.Value;
+                if (actionArgumentValue != null)
+                {
+                    var propertyInfo = message.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public).FirstOrDefault(
+                        p => p.Name.Equals(actionArgument.Key, StringComparison.InvariantCultureIgnoreCase)
+                             && actionArgumentValue.GetType().IsAssignableFrom(p.PropertyType));
+
+                    propertyInfo?.SetValue(message, actionArgumentValue);
+                }
+            }
         }
     }
 }
